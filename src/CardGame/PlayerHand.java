a1f56eed6e0807a6ef3b92c6a08a9a04462552fa
wendy <guojingt@sheridancollege.ca>
2020-04-12 21:30:46 -0400

    add PlayerHand class with attributes and constructor

---
+++ src/CardGame/PlayerHand.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package CardGame;
+
+/**
+ *
+ * @author wendy
+ */
+public class PlayerHand extends GroupOfCards {
+    private int size;
+    private String suit;
+    private Value value;
+    
+    
+    public PlayerHand(int size){
+        super(size);
+    }
+    
+    
+    
+}
