971d362ce3e45d108711ba9d8871a9ffafa74c49
wendy <guojingt@sheridancollege.ca>
2020-04-12 21:27:42 -0400

    add suit class with enum suits

---
+++ src/CardGame/Suit.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package CardGame;
+
+/**
+ *
+ * @author wendy
+ */
+public class Suit {
+
+    static Suit suits;
+        enum suits {
+        SPADES, 
+        HEART, 
+        CLUB, 
+        DIAMONDS
+    }
+}
