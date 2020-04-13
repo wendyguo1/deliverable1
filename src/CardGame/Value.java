09edf6fbd528da308131dd81cf6f2710f299efde
wendy <guojingt@sheridancollege.ca>
2020-04-12 21:28:41 -0400

    add Value class with enum values

---
+++ src/CardGame/Value.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package CardGame;
+
+/**
+ *
+ * @author wendy
+ */
+public class Value {
+
+    static Value values;
+        enum values {
+        ACE,
+        ONE,
+        TWO,
+        THREE,
+        FOUR,
+        FIVE,
+        SIX,
+        SEVEN,
+        EIGHT,
+        NINE,
+        TEN,
+        JACK,
+        QUEEN,
+        KING
+    }
+}
