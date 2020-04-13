5404dc7356dca7a05560a120fa8ff6c437da5307
wendy <guojingt@sheridancollege.ca>
2020-04-12 21:31:17 -0400

    add DrawPile class with attributes and constructor

---
+++ src/CardGame/DrawPile.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package CardGame;
+
+/**
+ *
+ * @author wendy
+ */
+public class DrawPile extends GroupOfCards {
+    private int size;
+    private String suit;
+    private Value value;
+    public DrawPile(int size){
+        super(size);
+    }
+}
